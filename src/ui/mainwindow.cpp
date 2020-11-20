c82afc1801d830039b2224a60b461b4e8282692d
Phobos Aryn'dythyrn D'thorga <phobos.gekko@gekkofyre.io>
2020-11-21 02:07:56 +1100

    * The spectrograph / waterfall now works in its entirety! I'm overwhelmed with emotion because following this path from the beginning involved a great amount of learning and personal growth, so I'm happy to be where this is right now. This applies to Issue #3.

--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -696,15 +696,6 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
             gkSpectroWaterfall->setDataDimensions(SPECTRO_X_MIN_AXIS_SIZE, SPECTRO_X_MAX_AXIS_SIZE, 64, AUDIO_FRAMES_PER_BUFFER);
         }
 
-        // Set the range only once (data range)...
-        static bool gkSetRangeOnlyOnce = true;
-        if (gkSetRangeOnlyOnce) {
-            double dataRng[2];
-            gkSpectroWaterfall->getDataRange(dataRng[0], dataRng[1]);
-            gkSpectroWaterfall->setRange(dataRng[0], dataRng[1]);
-            gkSetRangeOnlyOnce = false;
-        }
-
         //
         // Add the spectrograph / waterfall to the QMainWindow!
         ui->horizontalLayout_12->addWidget(gkSpectroWaterfall);
